7de7e8f933003ecf1eb2c47e083acdf6d8518007
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-01 21:49:17 +0100

    other usefull features

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 "use strict";
+var _a;
 let sales = 123456789;
 let course = 'TypeScript';
 let is_published = true;
@@ -25,4 +26,34 @@ let employee = {
         console.log(date);
     }
 };
+function kgToLbs(weight) {
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+kgToLbs(10);
+kgToLbs('10kg');
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function greet(name) {
+    if (name) {
+        console.log(name.toUpperCase);
+    }
+    else {
+        console.log('Hello!');
+    }
+}
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthday) === null || _a === void 0 ? void 0 : _a.getFullYear());
+let log = (message) => console.log(message);
+log === null || log === void 0 ? void 0 : log('a');
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -40,15 +40,103 @@ function calculateTax(income: number, taxYear: number = 2022): number {
 calculateTax(123)
 
 // Objects
-let employee: {
+// let employee: {
+//     readonly id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// } = {
+//     id: 1,
+//     name: 'Mosh',
+//     retire: (date: Date) => {
+//         console.log(date);
+
+//     }
+// };
+
+// Type Aliases
+type Employee = {
     readonly id: number,
     name: string,
     retire: (date: Date) => void
-} = {
+}
+
+let employee: Employee = {
     id: 1,
     name: 'Mosh',
     retire: (date: Date) => {
         console.log(date);
-
     }
 };
+
+// Union Types
+function kgToLbs(weight: number | string): number {
+    // Narrowing
+    if (typeof weight === 'number') {
+        return weight * 2.2;
+    }
+    else {
+        return parseInt(weight) * 2.2;
+    }
+}
+
+kgToLbs(10);
+kgToLbs('10kg');
+
+// Intersection Types
+type Draggable = {
+    drag: () => void;
+};
+
+type Resizable = {
+    resize: () => void
+};
+
+type UIWidget = Draggable & Resizable;
+let textBox: UIWidget = {
+    drag: () => { },
+    resize: () => { }
+}
+
+// Literal (exact, specific)
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+type Metric = 'cm' | 'inch';
+
+// Nullable types
+function greet(name: string | null | undefined) {
+    if (name) {
+        console.log(name.toUpperCase);
+    } else {
+        console.log('Hello!');
+    }
+}
+
+// greet(null)
+
+// Optional Chaining
+type Customer = {
+    birthday: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(1);
+// if (customer !== null && customer !== undefined) {
+//     console.log(customer.birthday);
+
+// }
+
+// Optional property access operator
+console.log(customer?.birthday?.getFullYear());
+
+// Optional element access operator
+// if (customers !== null && customers !== undefined)
+// customers[0]
+// customers?.[0]
+
+// Optional call
+let log: any = (message: string) => console.log(message);
+log?.('a')
