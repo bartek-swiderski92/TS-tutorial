476bdcd2e6541069fe96551fe01c097791831bcf
Bartek Swiderski <b.swiderski92@gmail.com>
2022-06-01 18:36:42 +0100

    enums

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,15 @@
 "use strict";
-let age = 20;
-if (age < 50)
-    age += 10;
-console.log(age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+let numbers = [1, 2, 3];
+let numbers1 = [];
+numbers1[0] = 1;
+numbers1[1] = 2;
+numbers.forEach(n => n.valueOf());
+let user = [1, 'Mosh'];
+;
+let mySize = 2;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,29 @@
-let age: number = 20;
-if (age < 50)
-    age += 10;
-console.log(age)
+let sales = 123_456_789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+
+
+let numbers = [1, 2, 3]
+
+let numbers1: number[] = []
+numbers1[0] = 1;
+// numbers1[1] = '2';
+numbers1[1] = 2;
+
+numbers.forEach(n => n.valueOf()) // vsc displays method available for type number
+
+let user: [number, string] = [1, 'Mosh'];
+
+
+// Enums
+
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+
+// PascalCase
+const enum Size { Small = 1, Medium, Large };
+let mySize: Size = Size.Medium;
+// console.log(mySize);
+
